a4f11d5ee7bebd96d176962cede96282383fd444
nullxDEADBEEF <andxdgu@gmail.com>
2022-03-08 21:06:45 +0100

    Switch to Axum from Tide and handle user not found

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -1,39 +1,55 @@
 mod document;
 mod routes;
-mod state;
 
-use state::SharedState;
+use axum::{extract::Extension, routing::get, Router};
+use mongodb::{options::ClientOptions, Client};
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
-async fn main() -> tide::Result<()> {
-//    let database = client.database("db");
-//    let users = database.collection::<User>("users");
-//
-//    // insert test data
-//    let test_user = User {
-//        name: String::from("Test"),
-//        phone: String::from("1234"),
-//        description: None,
-//    };
-//    users.insert_one(test_user, None).await?;
-//
-//    // print available databases
-//    println!("Databases:");
-//    for db_name in client.list_database_names(None, None).await? {
-//        println!("{}", db_name);
-//    }
-//
-//    // retrieve data from collection
-//    let user: User = users
-//        .find_one(doc! { "name": "Test" }, None)
-//        .await?
-//        .expect("Missing user with name 'Test'");
-//    println!("Retrieved {:?} from database 'db'", user);
-    tide::log::start();
-    let state = SharedState::new().await?;
-    let mut app = tide::with_state(state);
-    app.at("/find-user/:name").get(routes::find_user);
-    app.listen("localhost:9000").await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //    let database = client.database("db");
+    //    let users = database.collection::<User>("users");
+    //
+    //    // insert test data
+    //    let test_user = User {
+    //        name: String::from("Test"),
+    //        phone: String::from("1234"),
+    //        description: None,
+    //    };
+    //    users.insert_one(test_user, None).await?;
+    //
+    //    // print available databases
+    //    println!("Databases:");
+    //    for db_name in client.list_database_names(None, None).await? {
+    //        println!("{}", db_name);
+    //    }
+    //
+    //    // retrieve data from collection
+    //    let user: User = users
+    //        .find_one(doc! { "name": "Test" }, None)
+    //        .await?
+    //        .expect("Missing user with name 'Test'");
+    //    println!("Retrieved {:?} from database 'db'", user);
+    tracing_subscriber::fmt()
+        .with_env_filter("tower=trace")
+        .pretty()
+        .init();
+    let client_options = ClientOptions::parse("mongodb://root:example@localhost:27017").await?;
+    let db_client = Client::with_options(client_options)?.database("db");
+    let app = Router::new()
+        .route("/find-user/:name", get(routes::find_user))
+        // add 'db_client' to all request's extensions so handlers can access it.
+        .layer(Extension(db_client))
+        // logging of requests and responses
+        .layer(TraceLayer::new_for_http());
+
+    // run application with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+    println!("Listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
 
     Ok(())
 }
--- rust_api/src/routes.rs
+++ rust_api/src/routes.rs
@@ -1,23 +1,51 @@
-use tide::{Body, Request, Response};
-use mongodb::{bson::doc};
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use mongodb::bson::doc;
+use serde_json::json;
 
 use crate::document::User;
-use crate::state::SharedState;
 
-pub async fn find_user(req: Request<SharedState>) -> tide::Result<impl Into<Response>> {
-    let user_name = req.param("name")?;
-    let database = req.state().get_db().database("db");
-    let users = database.collection::<User>("users");
+pub enum UserError {
+    NotFound,
+}
 
-    let filter = doc! { "name": user_name };
-    // TODO: handle the case of a user not existing by
-    // returning a nice error message in JSON instead of panicking
-    let user: User = users
-        .find_one(filter, None)
-        .await?
-        .expect("Could not find a user with that name");
+// Handler for `GET /find-user/:name`
+pub async fn find_user(
+    Path(name): Path<String>,
+    Extension(db_client): Extension<mongodb::Database>,
+) -> Result<Json<User>, UserError> {
+    let users = db_client.collection::<User>("users");
 
-    let mut res = Response::new(200);
-    res.set_body(Body::from_json(&user)?);
-    Ok(res)
+    let filter = doc! { "name": name };
+    let user = match users.find_one(filter, None).await? {
+        Some(user) => user,
+        None => return Err(UserError::NotFound),
+    };
+
+    Ok(user.into())
+}
+
+// TODO: should probably not just return not found
+impl From<mongodb::error::Error> for UserError {
+    fn from(_error: mongodb::error::Error) -> Self {
+        UserError::NotFound
+    }
+}
+
+impl IntoResponse for UserError {
+    fn into_response(self) -> Response {
+        let (status, err_message) = match self {
+            UserError::NotFound => (StatusCode::NOT_FOUND, "User not found"),
+        };
+
+        let body = Json(json!({
+            "error": err_message,
+        }));
+
+        (status, body).into_response()
+    }
 }
